2a068c2bad343187c9fd94bb1cc5480179fe13ff
Antonio01-ui <antonio@codeeza.com>
2024-07-09 20:07:31 +0200

    First Commit

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://<username>:<password>@cluster0.ov6cuyu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+// Please create a free database for yourself.
+// I would advise you to use MONGODB as it's less complex.
